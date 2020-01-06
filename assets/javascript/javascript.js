bd5f490dbdaeb1ae6ace51744fb788fe7c1d5c43
Cake <mcgrail.shauna@gmail.com>
2020-01-05 23:54:34 -0700

    added weather image, and uv index

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -11,17 +11,15 @@ $(document).ready(function () {
     }, 1000);
 
     let citySearch;
-    const apiKey = "a2120d1ed5f7642121209ff5a7dc902b";
+    // const apiKey = "a2120d1ed5f7642121209ff5a7dc902b";
 
 
     $(".searchBtn").on("click", function () {
         event.preventDefault()
         citySearch = $(".cityInput").val();
-        // citySearch = $(this).prev().val();
         console.log(citySearch);
 
         const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + ",us&mode=json&units=imperial&appid=a2120d1ed5f7642121209ff5a7dc902b";
-
 
         $.ajax({
             url: queryURL,
@@ -33,8 +31,26 @@ $(document).ready(function () {
             $(".todayHumid").text((response.main.humidity) + "%");
             $(".todayWind").text((response.wind.speed) + "mph");
 
+            const weatherImage = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png");
+            $(".weatherIcon").append(weatherImage);
+
+            const lat = response.coord.lat;
+            const lon = response.coord.lon;
+            const uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=88ac7af34cb90d87533bbf879a2bb928&lat=" + lat + "&lon=" + lon;
+           
+            $.ajax({
+                url: uvURL,
+                method: "GET"
+            }).then(function (uvResponse) {
+                console.log(uvResponse);
+                $(".todayUv").text(uvResponse.value);
+            })
         });
+
 
     });
 
 });
+
+
+
