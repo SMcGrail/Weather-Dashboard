e5c502fba2b3822b7c3790159f044886d6055375
Cake <mcgrail.shauna@gmail.com>
2020-01-08 21:36:47 -0700

    added clickable links to recent searches

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
         const loadLat = (position.coords.latitude).toFixed(0);
         const loadLon = (position.coords.longitude).toFixed(0);
         console.log(loadLat, loadLon);
-        const loadURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + loadLat + "&lon=" + loadLon + "&mode=json&units=imperial&appid=a2120d1ed5f7642121209ff5a7dc902b";
+        const loadURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + loadLat + "&lon=" + loadLon + "&mode=json&units=imperial&appid=" + apiKey;
 
         $.ajax({
             url: loadURL,
@@ -33,7 +33,7 @@ $(document).ready(function () {
 
             const lat = response.coord.lat;
             const lon = response.coord.lon;
-            const uvURL = "https://api.openweathermap.org/data/2.5/uvi?appid=88ac7af34cb90d87533bbf879a2bb928&lat=" + loadLat + "&lon=" + loadLon;
+            const uvURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + loadLat + "&lon=" + loadLon;
 
             $.ajax({
                 url: uvURL,
@@ -50,13 +50,20 @@ $(document).ready(function () {
 
 
     //give weather for user selected location on search
+
     let citySearch;
     const searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
     console.log(searchHistory);
-    // const apiKey = "a2120d1ed5f7642121209ff5a7dc902b";
+    const apiKey = "a2120d1ed5f7642121209ff5a7dc902b";
 
 
     $(".searchBtn").on("click", function () {
+        searchWeather();
+    });
+
+
+
+    function searchWeather() {
         event.preventDefault();
         citySearch = $(".cityInput").val();
         console.log(citySearch);
@@ -64,8 +71,10 @@ $(document).ready(function () {
         searchHistory.unshift(citySearch);
         searchHistory.splice(5);
         localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
-        
-        const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + ",us&mode=json&units=imperial&appid=a2120d1ed5f7642121209ff5a7dc902b";
+        $(".recentList").empty();
+        setRecent();
+
+        const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + ",us&mode=json&units=imperial&appid=" + apiKey;
 
         $.ajax({
             url: queryURL,
@@ -82,7 +91,7 @@ $(document).ready(function () {
 
             const lat = response.coord.lat;
             const lon = response.coord.lon;
-            const uvURL = "https://api.openweathermap.org/data/2.5/uvi?appid=88ac7af34cb90d87533bbf879a2bb928&lat=" + lat + "&lon=" + lon;
+            const uvURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + lat + "&lon=" + lon;
 
             $.ajax({
                 url: uvURL,
@@ -95,7 +104,8 @@ $(document).ready(function () {
         });
 
 
-    });
+    };
+
 
     //function to add recent searches
     function setRecent() {
@@ -107,7 +117,14 @@ $(document).ready(function () {
             $(".recentList").append(recentListItem);
 
         }
+
+        $(".recentCity").on("click", function () {
+         citySearch = $(".cityInput").val(this);
+         searchWeather();
+        });
+    
     }
+
 
 });
 
